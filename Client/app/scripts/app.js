b1c545143ccc4ccf88320a8db8204e6039420df9
kaymorey <moreirakatia@neuf.fr>
2014-01-07 22:04:57 +0100

    displaying bubbles

--- Client/app/scripts/app.js
+++ Client/app/scripts/app.js
@@ -1,9 +1,113 @@
 $(function() {
 
-	var socket = io.connect('http://localhost:9001');
+    var socket = io.connect('http://localhost:9001');
 
-	socket.on('tweet', function (tweet, dataStats) {
-        $('.jumbotron .lead').html(tweet.text);
+    var SCREEN_WIDTH = window.innerWidth,
+        SCREEN_HEIGHT = window.innerHeight;
+
+    // Colors
+    var yellow = "#fab13b",
+    red = "#d10000",
+    green = "#70ad88",
+    blue = "#4bcddd",
+    background = "#d10000";
+
+    // Create paper Raphael
+    var paper = Raphael(document.getElementById("circles"), SCREEN_WIDTH * 2/3, SCREEN_HEIGHT / 2);
+
+    var particles = [];
+
+    var Particle = function (data) {
+        // Velocity
+        this.velX = Math.random() * 4 - 2;
+        this.velY = Math.random() * 2 + 0.5;
+
+        // Position
+        this.x = SCREEN_WIDTH / 2;
+        this.y = SCREEN_HEIGHT / 2;
+
+        // Random color
+        var color = Math.floor(Math.random() * 3 + 1);
+        switch(color) {
+                case 1: this.color = yellow; break;
+                case 2: this.color = blue; break;
+                case 3: this.color = green; break;
+                default: this.color = yellow;
+        }
+
+        // Length
+        this.radius = data.text.length / 3;
+
+        this.move = true;
+
+        this.data = data;
+    }
+
+    Particle.prototype = {
+        render: function() {
+            var text = this.data.text;
+            var id = this.data.id;
+            var data = this.data;
+
+            var circle = paper.circle(this.x, this.y, this.radius)
+            .attr('fill', this.color)
+            .attr('stroke', '#FFF')
+            .attr('stroke-width', 2);
+
+            var cx = Math.floor(Math.random() * SCREEN_WIDTH);
+            var cy = 0 - this.radius;
+            var speed = Math.floor(Math.random() * 2500 + 3000);
+            circle.animate({cx: cx, cy: cy}, speed, 'linear', function() {
+                    circle.remove();
+            });
+            circle.toBack();
+            
+            circle.hover(function() {
+                    circle.pause();
+                    circle.attr('stroke', '#FFF');
+                    circle.toFront();
+            },
+            function() {
+                    circle.animate({cx: cx, cy: cy}, speed, 'linear', function() {
+                            circle.remove();
+                    });
+                    circle.attr('stroke', '#FFF');
+                    $.fancybox.close();
+            })
+            circle.click(function() {
+                    var user = data.user;
+                    var template = '<div class="tweet">';
+                            template += '<img src ="'+user.profile_image_url+'" alt="" />';
+                            template += '<div class="user">';
+                                    template += '<span class="name">'+user.name+'</span><br />';
+                                    template += '<span class="screen-name">@'+user.screen_name+'</span>';
+                            template += '</div>';
+                            template += '<div class="clear"></div>';
+                            template += '<p class="text">'+data.text+'</p>';
+                    template += '</div>';
+                    $.fancybox.open( {
+                            content: template,
+                            closeBtn: false,
+                            autoSize: false,
+                            width: 500,
+                            height: 'auto',
+                            padding: 0,
+                            helpers: {
+                                    overlay: null
+                            }
+                    });
+            });
+        },
+        update: function() {
+            this.x += this.velX;
+            this.y -= this.velY;
+        }
+    }
+
+    socket.on('tweet', function (tweet, dataStats) {
+        var p = new Particle(tweet);
+        //particles.push(p);
+        p.render();
     });
 
 
